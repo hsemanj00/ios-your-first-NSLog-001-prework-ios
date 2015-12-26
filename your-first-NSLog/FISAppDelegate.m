f630204290e02950834acd3e564bd1cf6317c808
Henry Semanjarrez <hsemanj00@Henrys-iMac.fios-router.home>
2015-12-26 01:58:42 -0500

    2nd commit

--- your-first-NSLog/FISAppDelegate.m
+++ your-first-NSLog/FISAppDelegate.m
@@ -4,13 +4,16 @@
 
 @implementation FISAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
     /**
     
      * Hey! Write your code here! :D
      
      */
+    
+    NSLog(@"Hello, World!");
     
     // do not alter
     return YES;  //
